6b9d8b7170ba315ce70e537f650cb5910113ee06
Waqas Ahmad <waqas.stu@gmail.com>
2021-03-02 17:05:42 +0500

    Initial project set up commit
    DoTween integrated
    Deck, background configuration sprites added

---
+++ Assets/Scripts/CustomGrid.cs
@@ -0,0 +1,13 @@
+﻿public class CustomGrid
+{
+    private int width;
+    private int height;
+    private int[,] gridArray;
+
+    public CustomGrid(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+        gridArray = new int[width, height];
+    }
+}
